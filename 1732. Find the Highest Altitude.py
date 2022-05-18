fc7844c780c9b8856d3a2dec75f0c086df0b3aa7
Chetan <praveendatta9@gmail.com>
2022-05-18 23:23:26 +0530

    updated 1732

--- 1732. Find the Highest Altitude.py
+++ 1732. Find the Highest Altitude.py
@@ -1,6 +1,9 @@
-gain = [-4,-3,-2,-1,4,3,2]
-net_gain = [0]
+gain = [-5,1,5,0,-7]
+net_gain = 0
+ans = 0
 
 for i in range(len(gain)):
-    net_gain.append(net_gain[i] + gain[i])
-print(max(net_gain))
+     net_gain = net_gain + gain[i]
+     if net_gain >= ans:
+         ans = net_gain
+print(ans)
