bf3632ed784d0d8e41e3ee0245dad786c014aada
Chetan <praveendatta9@gmail.com>
2022-05-21 23:47:24 +0530

    solved 2011

--- 2011. Final Value of Variable After Performing Operations.py
+++ 2011. Final Value of Variable After Performing Operations.py
@@ -1,1 +1,9 @@
 operations = ['--x', 'x++', 'x++']
+x = 0
+
+for i in range(len(operations)):
+    if operations[i] == '--x' or operations[i] == 'x--':
+        x =  x-1
+    elif operations[i] == '++x' or operations[i] == 'x++':
+        x = x+1
+print(x)
