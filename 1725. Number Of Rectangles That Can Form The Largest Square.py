c8874e2b82823aca0ebe6a30369f441c7525d78d
Chetan <praveendatta9@gmail.com>
2022-05-22 22:32:34 +0530

    Optimized 1725

--- 1725. Number Of Rectangles That Can Form The Largest Square.py
+++ 1725. Number Of Rectangles That Can Form The Largest Square.py
@@ -1,14 +1,8 @@
 rectangles = [[5,8],[3,9],[5,12],[16,5]]
-squares = []
 maxlength = []
 
 for i in range(len(rectangles)):
-    val = [min(rectangles[i]),min(rectangles[i])]
-    squares.append(val)
-
-for i in range(len(squares)):
-    val1 = max(squares[i])
-    maxlength.append(val1)
+    maxlength.append(min(rectangles[i]))
 
 length = max(maxlength)
 count = 0
