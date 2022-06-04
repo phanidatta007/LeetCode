99886af359d0a8961f90afb5c74eb73630fa1aca
Chetan <praveendatta9@gmail.com>
2022-06-04 23:23:03 +0530

    Solved 2176

---
+++ 2176. Count Equal and Divisible Pairs in an Array.py
@@ -0,0 +1,13 @@
+nums = [3,1,2,2,2,1,3]
+k = 2
+n = len(nums)
+count = 0
+
+for i in range(n):
+    for j in range(i+1,n):
+        if nums[i] == nums[j] and (i*j) % k == 0:
+            count += 1
+        else:
+            count += 0
+
+print(count)
