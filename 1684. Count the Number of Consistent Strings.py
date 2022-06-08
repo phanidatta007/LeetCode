21b600140f0ce7367bff84ff31c7eba13c64fa98
Chetan <praveendatta9@gmail.com>
2022-06-08 23:08:55 +0530

    solved 1684 problem

--- 1684. Count the Number of Consistent Strings.py
+++ 1684. Count the Number of Consistent Strings.py
@@ -1,8 +1,15 @@
 allowed = "ab"
 words = ["ab","bd","aaab","baa","badab"]
 count = 0
-for i in range(len(words)):
-    for j in range(len(words[i])):
-        if allowed == words[j]:
-            count += 1
+
+def isBuilt(allowed, word):
+    for i in range(len(word)):
+        if word[i] not in allowed:
+            return False
+
+    return True
+
+for word in words:
+    if isBuilt(allowed, word):
+        count += 1
 print(count)
